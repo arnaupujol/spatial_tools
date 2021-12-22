6371f3492623d2ea714adf847469df7f008a599f
Arnau Pujol <arnaupv@gmail.com>
2021-12-22 16:43:17 +0100

    spatial_tools/fof.get_fof_PR() added

--- spatial_tools/fof.py
+++ spatial_tools/fof.py
@@ -99,3 +99,144 @@ def get_fofid(positions, scale):
     for i, f in enumerate(np.unique(fofid[fofid>0])):
         fofid[fofid == f] = i+1
     return fofid
+
+def get_fof_PR(positions, test_result, scale, fofid = None):
+    """
+    This method runs the Friends of Friends (FOF) algorithm (if fofid is None) and obtains the mean
+    positivity rate (PR) of each FOF extended with the non-infected cases closer than the scale.
+
+    Parameters:
+    -----------
+
+    Parameters:
+    -----------
+    positions: np.ndarray
+        An array with the position parameters with shape (n,2),
+        where n is the number of positions
+    test_result: np.array
+        An array with the test results (0 or 1)
+    scale: float
+        The linking scale of the FoF
+    fofid: np.array
+        An array with the FOF ids of the positive cases
+
+    Returns:
+    --------
+    fofid: np.array
+        List of the FoF IDs of each position, with 0 for those
+        without a FoF.
+    mean_pr_fof: np.array
+        Mean PR corresponding to fofid
+    extended_fofid: np.array
+        FOF IDs of extended groups including non-infected friends
+    mean_pr_ext_fof: np.array
+        Mean PR corresponding to extended_fofid
+    """
+    #Define positions of positive cases
+    positive_positions = positions[test_result == 1]
+    #Computing fofid is needed
+    if fofid is None:
+        fofid = fof.get_fofid(positive_positions, scale)
+    #Create KDTree for all populations
+    tree =spatial.KDTree(positions)
+
+    #Mean PR for the positive cases in FOFs
+    mean_pr_fof = np.zeros_like(fofid)
+    #Mean PR for all positions cases in extended FOFs
+    mean_pr_ext_fof = np.zeros_like(test_result)
+    #ID of the extended FOFs
+    extended_fofid = np.zeros_like(test_result)
+    for i,f in enumerate(np.unique(fofid[fofid>0])):
+        #get all indeces with this FOF id
+        has_this_fofid = fofid == f
+        fofid_indeces = np.arange(len(positive_positions))[has_this_fofid]
+        #for all these indeces, get list of friends from all positions
+        all_friends_indeces = fof.get_friends_indeces(positive_positions[fofid_indeces], scale, tree)
+        #get unique values of such indeces
+        total_friends_indeces = np.unique(np.concatenate(all_friends_indeces))
+        #get mean infection from all the unique indeces
+        mean_pr = np.mean(test_result[total_friends_indeces])
+        #assign mean PR to each fofid for the positive cases
+        mean_pr_fof[fofid_indeces] = mean_pr
+        #assign mean PR to each extended fofid
+        mean_pr_ext_fof[total_friends_indeces] = mean_pr
+        #assign FOF id to non-infected positions
+        extended_fofid[total_friends_indeces] = f
+    return fofid, mean_pr_fof, extended_fofid, mean_pr_ext_fof
+
+def get_fof_PR(positions, test_result, scale, fofid = None):
+    """
+    This method runs the Friends of Friends (FOF) algorithm (if fofid is None) and obtains the mean
+    positivity rate (PR) of each FOF extended with the non-infected cases closer than the scale.
+
+    Parameters:
+    -----------
+
+    Parameters:
+    -----------
+    positions: np.ndarray
+        An array with the position parameters with shape (n,2),
+        where n is the number of positions
+    test_result: np.array
+        An array with the test results (0 or 1)
+    scale: float
+        The linking scale of the FoF
+    fofid: np.array
+        An array with the FOF ids of the positive cases
+
+    Returns:
+    --------
+    fofid: np.array
+        List of the FoF IDs of each position, with 0 for those
+        without a FoF.
+    mean_pr_fof: np.array
+        Mean PR corresponding to fofid
+    fof_catalogue: dict
+        Dictionary of a catalogue of the FOF groups and its main characteristics
+    """
+    #Define positions of positive cases
+    positive_positions = positions[test_result == 1]
+    #Computing fofid is needed
+    if fofid is None:
+        fofid = fof.get_fofid(positive_positions, scale)
+    #Create KDTree for all populations
+    tree =spatial.KDTree(positions)
+
+    #Mean PR for the positive cases in FOFs
+    mean_pr_fof = np.zeros_like(fofid)
+    #FOF catalogue
+    fof_catalogue = {'id' : [], #FOF id
+                     'mean_pos' : [], #Mean position of positive cases
+                     'mean_pos_ext' : [], #Mean position of extended FOF
+                     'mean_pr' : [], #Positivity rate
+                     'positives' : [], #Number of positive cases
+                     'negatives' : [], #Number of negative cases in extended FOF
+                     'total' : [], #Total number of positions in extended FOF
+                     'indeces' : [], #Indeces of all positions in extended FOF
+                    }
+    for i,f in enumerate(np.unique(fofid[fofid>0])):
+        #get all indeces with this FOF id
+        has_this_fofid = fofid == f
+        fofid_indeces = np.arange(len(positive_positions))[has_this_fofid]
+        #for all these indeces, get list of friends from all positions
+        all_friends_indeces = fof.get_friends_indeces(positive_positions[fofid_indeces], scale, tree)
+        #get unique values of such indeces
+        total_friends_indeces = np.unique(np.concatenate(all_friends_indeces))
+        #get mean infection from all the unique indeces
+        mean_pr = np.mean(test_result[total_friends_indeces])
+        #assign mean PR to each fofid for the positive cases
+        mean_pr_fof[fofid_indeces] = mean_pr
+        #assign mean PR to each extended fofid
+        mean_pr_ext_fof[total_friends_indeces] = mean_pr
+        #assign FOF id to non-infected positions
+        extended_fofid[total_friends_indeces] = f
+        #setting FOF catalogue
+        fof_catalogue['id'].append(f)
+        fof_catalogue['mean_pos'].append(np.mean(positive_positions[fofid_indeces], axis = 0))
+        fof_catalogue['mean_pos_ext'].append(np.mean(positions[total_friends_indeces], axis = 0))
+        fof_catalogue['mean_pr'].append(mean_pr)
+        fof_catalogue['positives'].append(len(fofid_indeces))
+        fof_catalogue['negatives'].append(len(total_friends_indeces) - len(fofid_indeces))
+        fof_catalogue['total'].append(len(total_friends_indeces))
+        fof_catalogue['indeces'].append(total_friends_indeces)
+    return fofid, mean_pr_fof, fof_catalogue
