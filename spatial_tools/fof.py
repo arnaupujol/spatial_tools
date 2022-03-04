3e78427ff86e14d76090fccf5bab1b56370fd92c
Arnau Pujol <arnaupv@gmail.com>
2022-03-04 14:25:37 +0100

    min_neighbours parameter added in spatial_tools/fof.get_fofid/get_fof_PR and other functions added for tempID and temporal FOF analysis

--- spatial_tools/fof.py
+++ spatial_tools/fof.py
@@ -44,7 +44,7 @@ def get_friends_indeces(positions, scale, tree):
         indeces[i] = np.array(indeces[i], dtype = int)
     return indeces
 
-def get_fofid(positions, scale):
+def get_fofid(positions, scale, min_neighbours = 2):
     """
     This method finds the Friends of Friends (FoF) groups from
     a set of positions and returns their FoF IDs.
@@ -56,6 +56,9 @@ def get_fofid(positions, scale):
         where n is the number of positions
     scale: float
         The linking scale of the FoF
+    min_neighbours: int
+        Minium number of neighbours in the radius < scale needed to consider
+        the FOF group
 
     Returns:
     --------
@@ -74,7 +77,7 @@ def get_fofid(positions, scale):
     last_fofid = 0
     for i in range(len(positions)):
         #check if ith position has any neighbour
-        if len(indeces[i]) == 1:
+        if len(indeces[i]) < min_neighbours:
             continue
         else:
             #Define indeces of selected friends
@@ -105,7 +108,7 @@ def get_fofid(positions, scale):
         fofid[fofid == f] = i+1
     return fofid
 
-def get_fof_PR(positions, test_result, scale, fofid = None):
+def get_fof_PR(positions, test_result, scale, fofid = None, min_neighbours = 2):
     """
     This method runs the Friends of Friends (FOF) algorithm (if fofid is None) and obtains the mean
     positivity rate (PR) of each FOF extended with the non-infected cases closer than the scale.
@@ -124,6 +127,9 @@ def get_fof_PR(positions, test_result, scale, fofid = None):
         The linking scale of the FoF
     fofid: np.array
         An array with the FOF ids of the positive cases
+    min_neighbours: int
+        Minium number of neighbours in the radius < scale needed to consider
+        the FOF group
 
     Returns:
     --------
@@ -139,7 +145,8 @@ def get_fof_PR(positions, test_result, scale, fofid = None):
     positive_positions = positions[test_result == 1]
     #Computing fofid is needed
     if fofid is None:
-        fofid = get_fofid(positive_positions, scale)
+        fofid = get_fofid(positive_positions, scale, \
+                            min_neighbours = min_neighbours)
     #Create KDTree for all populations
     tree =spatial.KDTree(positions)
 
@@ -206,6 +213,151 @@ def fof2geodf(fof_catalogue, epsg = 3857):
     fof_geocat = pd.DataFrame(fof_catalogue)
     x_points = np.array([i[0] for i in fof_geocat['mean_pos']])
     y_points = np.array([i[1] for i in fof_geocat['mean_pos']])
-    fof_geocat = geopandas.GeoDataFrame(fof_geocat, geometry = geopandas.points_from_xy(x_points, y_points))
+    fof_geocat = geopandas.GeoDataFrame(fof_geocat, \
+                        geometry = geopandas.points_from_xy(x_points, y_points))
     fof_geocat = fof_geocat.set_crs(epsg=epsg)
     return fof_geocat
+
+def get_dist(pos_a, pos_b):
+    """
+    This method calculates the Euclidean distance
+    between two positions.
+
+    Parameters:
+    -----------
+    pos_a: np.ndarray
+        First position
+    pos_b: np.ndarray
+        Second position
+
+    Returns:
+    --------
+    dist: float
+        Distance between positions
+    """
+    dist = np.sqrt(np.sum((pos_a - pos_b)**2))
+    return dist
+
+def get_temp_id(fof_cat_list, linking_time, linking_dist, get_timelife = True):
+    """
+    This method generates the temporal ID of hotspots by linking
+    hotspots from different time frames, assigning the same
+    temporal ID to them when they are close enough in time and space.
+
+    Parameters:
+    -----------
+    fof_cat_list: list of pandas.DataFrame
+        List of hotspot catalogues, each element of the list
+        corresponding to the hotspot catalogue of each timestep
+    linking_time: int
+        Maximum number of timesteps of distance to link hotspots with
+        the same temporal ID
+    linking_dist: float
+        Linking distance used to link the hotspots from the different
+        time frames
+    get_timelife: bool
+        It specifies if time periods and timelife of hotspots is obtained
+
+    Returns:
+    --------
+    fof_cat_list: list of pandas.DataFrame
+        List of hotspot catalogues with the added variable 'tempID'
+    """
+    #setting empty values of temp_id
+    for t in range(len(fof_cat_list)):
+        fof_cat_list[t]['tempID'] = pd.Series(dtype = int)
+    #Initialising tempID value to assign
+    next_temp_id = 0
+    #Loop over all timesteps
+    for t in range(len(fof_cat_list)):
+        #Loop over all FOFs in a timestep
+        for f in fof_cat_list[t].T:
+            #Loop over all timesteps within linking_time
+            for t2 in range(t + 1, min(t + linking_time, len(fof_cat_list))):
+                #Loop over all FOFs in the linked timesteps
+                for f2 in fof_cat_list[t2].T:
+                    #Calculating distance between hotspots
+                    dist = get_dist(fof_cat_list[t].loc[f]['mean_pos'], \
+                                    fof_cat_list[t2].loc[f2]['mean_pos'])
+                    if dist <= scale:
+                        temp_id1 = fof_cat_list[t].loc[f]['tempID']
+                        temp_id2 = fof_cat_list[t2].loc[f2]['tempID']
+                        #Assign tempIDs to linked hotspots
+                        if np.isnan(temp_id1) and np.isnan(temp_id2):
+                            fof_cat_list[t]['tempID'].loc[f] = next_temp_id
+                            fof_cat_list[t2]['tempID'].loc[f2] = next_temp_id
+                            next_temp_id += 1
+                        elif np.isnan(temp_id1):
+                            fof_cat_list[t]['tempID'].loc[f] = temp_id2
+                        elif np.isnan(temp_id2):
+                            fof_cat_list[t2]['tempID'].loc[f2] = temp_id1
+                        elif temp_id1 != temp_id2:
+                            for t3 in range(len(fof_cat_list)):
+                                fof_cat_list[t3]['tempID'].loc[fof_cat_list[t3]['tempID'] == temp_id2] = temp_id1
+    if get_timelife:
+        fof_cat_list = get_lifetimes(fof_cat_list)
+    return fof_cat_list
+
+def get_lifetimes(fof_cat_list):
+    """
+    This method obtains the first and last time frames for each
+    temporal ID from a list of hotspot catalogues and the corresponding
+    timelife.
+
+    Parameters:
+    -----------
+    fof_cat_list: list of pandas.DataFrame
+        List of hotspot catalogues, each element of the list
+        corresponding to the hotspot catalogue of each timestep
+
+    Returns:
+    --------
+    fof_cat_list: list of pandas.DataFrame
+        List of hotspot catalogues with the added fields 'first_timestep',
+        'last_timestep' and 'lifetime'
+    """
+    #getting list of timeIDs appearing in fof_cat_list
+    timeid_list = get_label_list(fof_cat_list, label = 'tempID')
+    #Creating empty columns for first timestep, last timestep and lifteime
+    for t in range(len(fof_cat_list)):
+            fof_cat_list[t]['first_timestep'] = pd.Series(dtype = int)
+            fof_cat_list[t]['last_timestep'] = pd.Series(dtype = int)
+            fof_cat_list[t]['lifetime'] = 0
+    for timeid_num in timeid_list:
+        appearances = []
+        for i in range(len(fof_cat_list)):
+            if timeid_num in fof_cat_list[i]['tempID'].unique():
+                appearances.append(i)
+        min_appearance = min(appearances)
+        max_appearance = max(appearances)
+        lifetime = max_appearance - min_appearance
+        for i in range(min_appearance, max_appearance + 1):
+            fof_cat_list[i]['first_timestep'].loc[fof_cat_list[i]['tempID'] == timeid_num] = min_appearance
+            fof_cat_list[i]['last_timestep'].loc[fof_cat_list[i]['tempID'] == timeid_num] = max_appearance
+            fof_cat_list[i]['lifetime'].loc[fof_cat_list[i]['tempID'] == timeid_num] = lifetime
+    return fof_cat_list
+
+def get_label_list(df_list, label = 'tempID'):
+    """
+    This method gives the unique values of a column in a list
+    of data frames.
+
+    Parameters:
+    -----------
+    df_list: list of pandas.DataFrames
+        List of dataframes
+    label: str
+        Name of column to select
+
+    Returns:
+    --------
+    label_list: list
+        List of unique values of the column over all dataframes
+    """
+    for i in range(len(df_list)):
+        mask = df_list[i][label].notnull()
+        if i == 0:
+            label_list = df_list[i][label].loc[mask].unique()
+        else:
+            label_list = np.unique(np.concatenate((label_list, df_list[i][label].loc[mask].unique())))
+    return label_list
